eb43c7c95e8c8aba232668fa9442f4d73d1ec8db
Pathlib <gautroncyber@proton.me>
2026-03-22 23:46:20 +0100

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 data = {
     "nom": ["jone", "marc", "eliot"],
@@ -6,6 +7,7 @@ data = {
     "travaille": ["informatique", "maçon", "ingénieur"]
 }
 
+f=np.array([50,60,70,80])
 e = pd.DataFrame(data)
 
 print(e.info())        
@@ -16,4 +18,5 @@ e["date"] = pd.to_datetime(["2020", "2021", "2022"])
 
 e.set_index("date", inplace=True)  
 print(e)
+print(f)
 e.to_excel()
